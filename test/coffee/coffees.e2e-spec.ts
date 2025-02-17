fe626a0235fc603a0a2c36e2791ebff3a12254d1
Rubén Carrasco <rubencf@ext.inditex.com>
2025-02-17 13:15:00 +0100

    chore: coffees e2e test

--- test/coffee/coffees.e2e-spec.ts
+++ test/coffee/coffees.e2e-spec.ts
@@ -1,10 +1,27 @@
-import { INestApplication } from '@nestjs/common';
+import { HttpStatus, INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CreateCoffeeDto } from 'src/coffees/dto/create-coffee.dto';
+import { UpdateCoffeeDto } from 'src/coffees/dto/update-coffee.dto';
+import * as request from 'supertest';
 import { CoffeesModule } from '../../src/coffees/coffees.module';
 
 describe('[Feature] Coffees - /coffees', () => {
+  const coffee = {
+    name: 'Shipwreck Roast',
+    brand: 'Buddy Brew',
+    flavors: ['chocolate', 'vanilla'],
+  };
+  
+  const expectedPartialCoffee = expect.objectContaining({
+    ...coffee,
+    flavors: expect.arrayContaining(
+      coffee.flavors.map(name => expect.objectContaining({ name })),
+    ),
+  });
+
   let app: INestApplication;
+  let id = '';
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -24,14 +41,80 @@ describe('[Feature] Coffees - /coffees', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        transform: true,
+        forbidNonWhitelisted: true,
+        transformOptions: {
+          enableImplicitConversion: true,
+        }
+      })
+    );
     await app.init();
   });
 
-  it.todo('Create [POST /]');
-  it.todo('Get all [GET /]');
-  it.todo('Get one [GET /:id]');
-  it.todo('Update one [PATCH /:id]');
-  it.todo('Delete one [DELETE /:id]');
+  it('Create [POST /]', () => {
+    return request(app.getHttpServer())
+      .post('/coffees')
+      .send(coffee as CreateCoffeeDto)
+      .expect(HttpStatus.CREATED)
+      .then(({ body }) => {
+        expect(body).toEqual(expectedPartialCoffee);
+        id = body.id;
+      })
+  });
+
+  it('Get all [GET /]', () => {
+    return request(app.getHttpServer())
+      .get('/coffees')
+      .send(coffee as CreateCoffeeDto)
+      .expect(HttpStatus.OK)
+      .then(({ body }) => {
+        expect(body).toEqual([expectedPartialCoffee]);
+      })
+  });
+
+  it('Get one [GET /:id]', () => {
+    return request(app.getHttpServer())
+      .get(`/coffees/${id}`)
+      .send(coffee as CreateCoffeeDto)
+      .expect(HttpStatus.OK)
+      .then(({ body }) => {
+        expect(body).toEqual(expectedPartialCoffee);
+      })
+  });
+
+  it('Update one [PATCH /:id]', () => {
+    const updateCoffeeDto: UpdateCoffeeDto = {
+      ...coffee,
+      name: 'New and Improved Shipwreck Roast'
+    }
+    return request(app.getHttpServer())
+      .patch(`/coffees/${id}`)
+      .send(updateCoffeeDto)
+      .expect(HttpStatus.OK)
+      .then(({ body }) => {
+        expect(body.name).toEqual(updateCoffeeDto.name);
+        return request(app.getHttpServer())
+          .get(`/coffees/${id}`)
+          .then(({ body }) => {
+            expect(body.name).toEqual(updateCoffeeDto.name);
+          });
+      })
+  });
+
+  it('Delete one [DELETE /:id]', () => {
+    return request(app.getHttpServer())
+      .delete(`/coffees/${id}`)
+      .send(coffee as CreateCoffeeDto)
+      .expect(HttpStatus.OK)
+      .then(() => {
+        return request(app.getHttpServer())
+          .get(`/coffees/${id}`)
+          .expect(HttpStatus.NOT_FOUND);
+      });
+  });
 
   afterAll(async () => {
     await app.close();
